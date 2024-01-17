67c398ec834b592b8e4ed1786c685e2fe9ab4dd8
S3CORP\phuong.trinh <phuong.trinh@s3corp.com.vn>
2024-01-17 10:14:10 +0700

    Completed lab 2

--- src/labs/Lab2_1.java
+++ src/labs/Lab2_1.java
@@ -12,21 +12,21 @@ public class Lab2_1 {
         double weight = scanner.nextDouble();
 
         double bmi = weight / (height * 2);
-
+        double weightChange = Math.abs(24.9 - bmi);
 
         if (bmi < 18.5) {
-            System.out.println("Ban bi thieu can!");
-            System.out.println("Ban can phai an nhieu hon");
+            System.out.println("Ban co the dang bi thieu can!");
+            System.out.println("De duy tri can nang, ban co the can phai an nhieu hon. So can con thieu cua ban la: " + weightChange + " kg.");
         } else if (18.5 <= bmi && bmi <= 24.9) {
-            System.out.println("Than hinh ban that can doi");
+            System.out.println("Than hinh cua ban co ve can doi.");
         } else if (25 <= bmi && bmi <= 29.9) {
-            System.out.println("Ban dang thua can");
-            System.out.println("Ban can phai choi the thao");
+            System.out.println("Ban co the dang thua can.");
+            System.out.println("De giu can nang ly tuong, co the ban nen thuc hien the thao. So can du thua cua ban la: " + weightChange + " kg.");
         } else {
-            System.out.println("Ban dang bi beo phi");
-            System.out.println("SOS");
+            System.out.println("Ban co the dang bi beo phi.");
+            System.out.println("Xin vui long tham khao y kien bac si. So can du thua cua ban la: " + weightChange + " kg.");
         }
 
-        System.out.printf("Your BMI is: %.2f\n", bmi);
+        System.out.printf("Chi so BMI cua ban la: %.2f\n", bmi);
     }
 }
